8577601a02a6dbd3f5101e0cf6eb85e856d052e8
Stephanie Velazquez <stephanievelazquez@Stephanies-MacBook-Pro-2.local>
2016-08-01 10:40:05 -0700

    [FEATURE] DB started

--- server/server.js
+++ server/server.js
@@ -10,4 +10,8 @@ app.listen(app.get('port'), function() {
     console.log('Server listening on port ', app.get('port'));
 });
 
+app.use(function(req, res) {
+ res.send('404: Page not Found', 404);
+});
+
 module.exports = app;
