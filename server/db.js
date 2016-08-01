bf23bf2ac910692f36873740400c98fce66da40a
Stephanie Velazquez <stephanievelazquez@Stephanies-MacBook-Pro-2.local>
2016-08-01 16:11:44 -0700

    [FEATURE] Started authentication and installed bcrypt

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 var Sequelize = require('sequelize');
 var _ = require('lodash');
+var bcrypt = require('bcrypt');
 
 var sequelize = newSequelize('postgres://admin:MKIIYOLGKLYYSDMR@aws-us-east-1-portal.9.dblayer.com:11193/compose');
 
