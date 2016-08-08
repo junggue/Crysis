9da99aee350c856d48f271499cc2081cb439fb1b
Stephanie Velazquez <stephanievelazquez@Stephanies-MacBook-Pro-2.local>
2016-08-08 13:57:16 -0700

    [DDEBUG] Moved password verification fn to separate file

---
+++ server/utils/fnHelper.js
@@ -0,0 +1,14 @@
+var db = require('../db/db.js');
+var bcrypt = require('bcrypt');
+
+//Verify password matches on login
+exports.verifyPassword = function(password, enteredPw) {
+	return new Promise(function(resolve, reject) {
+    bcrypt.compare(enteredPw, password, function(err, res) {
+      if (err) {
+				return reject(err);
+			}
+      resolve(res);
+    });
+  });
+}
