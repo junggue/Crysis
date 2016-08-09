3eabd1dfefced23630bad207c1b2a743d83ff570
junggue <junggue.yang@gmail.com>
2016-08-09 09:38:06 -0700

    [DEBUG] login controller

--- server/utils/fnHelper.js
+++ server/utils/fnHelper.js
@@ -4,16 +4,11 @@ var bcrypt = require('bcrypt');
 //Verify password matches on login
 exports.verifyPassword = function(password, enteredPw) {
 	return new Promise(function(resolve, reject) {
-    console.log('in promise in verify ', password, enteredPw);
-    bcrypt.compare(password, enteredPw, function(err, res) {
+    bcrypt.compare(enteredPw, password, function(err, res) {
       if (err) {
-        console.log('password ', password);
-        console.log('attemtped ', enteredPw);
-        console.log('err in verifying pw ', err);
 				return reject(err);
 			}
-      resolve(res);
-      console.log('res in verify pw ', res);
+      return resolve(res);
     });
   });
 }
--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -3,14 +3,15 @@ var secret = require('../env/config.js')['key'];
 
 module.exports.tokenCheck = function(req, res, next){
 
-	var token = req.headers['x-access-token'];
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
 	if (token) {
-		jwt.verify(token, secret, function(err, decoded){
+		console.log("MIDDLEWARE")
+		jwt.verify(token, secret.SECRET, function(err, decoded){
 			if (err) {
 				return res.send(err.message);
 			} else {
-				next()
+				next();
 			}
 		});
 	} else {
