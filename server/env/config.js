2f9bf5eee3fe2aa33c8d3a0ac89e7c736f1cdb2f
junggue <junggue.yang@gmail.com>
2016-08-22 07:56:04 -0700

    test

---
+++ server/env/config.js
@@ -0,0 +1,12 @@
+module.exports.postgres = {
+	databaseName: 'compose',
+	userName: 'admin',
+	password: 'GGMJXMLEQKCGRJDG',
+	host: 'haproxy859.aws-us-east-1-portal.12.dblayer.com',
+	port: 10859,
+	dialect: 'postgres'
+};
+
+module.exports.key = {
+	SECRET: 'RANDOM_STRING'
+}
